b5f2c2c839d89a73d32872feb4751f3ce59634c9
Eren-Yeag <esenbaevnasyr@gmail.com>
2022-06-21 15:41:56 +0300

    task about writing code-3

--- script.js
+++ script.js
@@ -43,15 +43,88 @@
 
 // ВЛОЖЕННЫЕ ЦИКЛЫ
 
-let result = '';
-const length = 7;
-for (let i = 1; i < length; i++) {
-    for (let j = 0; j < i; j++) {
-        result += '*'
-    }
-    result += '\n'
-}
+// let result = '';
+// const length = 10;
+// for (let i = 1; i < length; i++) {
+//     for (let j = 0; j < i; j++) {
+//         result += '*'
+//     }
+//     result += '\n'
+// }
+// console.log(result);
 
-console.log(result)
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 3; k++) {
+//             if (k === 2) continue first;
+//             console.log(`Third level: ${k}`)
+//         }
+//     }
+// }
+//
+//
+// function firstTask() {
+//     for (let i = 5; i < 11; i++) {
+//         console.log(i)
+//     }
+// }
+// firstTask();
+// // Место для второй задачи
+// function secondTask() {
+//     for (let i = 20; i >= 10; i--) {
+//         if (i === 13) break;
+//         console.log(i)
+//     }
+// }
+// secondTask()
+// // Место для третьей задачи
+// function thirdTask() {
+//     for (let i = 2; i <= 10; i++) {
+//         if (i % 2 === 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+// thirdTask()
+// // Место для четвертой задачи
+//
+// // Цикл, который нужно переписать:
+//
+// // for (let i = 2; i <= 16; i++) {
+// //     if (i % 2 === 0) {
+// //         continue;
+// //     } else {
+// //         console.log(i);
+// //     }
+// // }
+//
+// function fourthTask() {
+//     let h = 2;
+//     while(h <= 16) {
+//         if (h % 2 === 0) {
+//             h++;
+//             continue;
+//         } else {
+//             console.log(h)
+//         }
+//         h++
+//     }
+// }
+// fourthTask();
+// // Место для пятой задачи
+// function fifthTask() {
+//     const arrayOfNumbers = [];
+//     // Пишем решение вот тут
+//     for (let i = 5; i < 10; i++) {
+//         arrayOfNumbers.push(i)
+//     }
+//
+// // Не трогаем
+//     return arrayOfNumbers;
+// }
+//
+// console.log(fifthTask());
 
 // ВЛОЖЕННЫЕ ЦИКЛЫ
