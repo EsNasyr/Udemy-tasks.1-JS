189d456f056a80a620b126dbd92caa36c69df563
Eren-Yeag <esenbaevnasyr@gmail.com>
2022-06-21 14:02:16 +0300

    solved task lesson-23

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ЛОГИЧЕСКИЕ ОПЕРАТОРЫ
+
 // console.log( NaN || 2 || undefined ); //2
 // console.log( NaN && 2 && undefined ); //NaN
 // console.log( 1 && 2 && 3 ); //3
@@ -25,17 +27,31 @@
 // if (hamburger || cola || fries === 3 || nuggets) {
 //     console.log('Done!')
 // } //Done!
+//
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+//
+// if (hamburger && cola || fries === 3 && nuggets) {
+//     console.log('Done!')
+// } else {
+//     console.log('!Done')
+// }
 
-let hamburger;
-const fries = NaN;
-const cola = 0;
-const nuggets = 2;
+// ЛОГИЧЕСКИЕ ОПЕРАТОРЫ
 
-if (hamburger && cola || fries === 3 && nuggets) {
-    console.log('Done!')
-} else {
-    console.log('!Done')
+// ВЛОЖЕННЫЕ ЦИКЛЫ
+
+let result = '';
+const length = 7;
+for (let i = 1; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+        result += '*'
+    }
+    result += '\n'
 }
 
+console.log(result)
 
-
+// ВЛОЖЕННЫЕ ЦИКЛЫ
